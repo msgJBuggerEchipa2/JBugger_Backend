48fefa516642289acc2670e4b830bed2e5affe4f
stefnmUBB <liviu.neacsu@stud.ubbcluj.ro>
2023-12-17 15:04:52 +0200

    added auto suspend user after 5 failed logins

--- app/src/main/java/com/msgsystems/jbugger/echipa2/backend/auth/AuthenticationService.java
+++ app/src/main/java/com/msgsystems/jbugger/echipa2/backend/auth/AuthenticationService.java
@@ -6,8 +6,11 @@ import com.msgsystems.jbugger.echipa2.backend.repository.UserRepository;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 @Service
 public class AuthenticationService {
@@ -34,18 +37,35 @@ public class AuthenticationService {
         return userRepository.save(user);
     }
 
+    private final HashMap<String, Integer> failedLoginAttempts = new HashMap<>();
+
     public User authenticate(LoginUserDto input) {
         System.out.println("Authint = ");
-        var auth = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(
-                        input.getUsername(),
-                        input.getPassword()
-                )
-        );
-        System.out.println("Auth result = "+auth.toString());
+        var username = input.getUsername();
+        var password = input.getPassword();
 
-        return userRepository.findByUsername(input.getUsername())
-                .orElseThrow();
+        var user = userRepository.findByUsername(username).orElseThrow();
+        if(!user.isEnabled())
+            throw new DeactivatedUserException();
+
+        try {
+            var auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
+            System.out.println("Auth result = "+auth.toString());
+        }
+        catch (AuthenticationException e) {
+            failedLoginAttempts.put(
+                    username, failedLoginAttempts.getOrDefault(username, 0) + 1
+            );
+            if(failedLoginAttempts.get(username)>=5){
+                user.deactivateUser();
+                userRepository.save(user);
+                throw new DeactivatedUserException();
+            }
+            throw e;
+        }
+        return user;
     }
 
     public void assertPermission(Authentication auth, String permissionType) throws NotPermittedException {
--- app/src/main/java/com/msgsystems/jbugger/echipa2/backend/controllers/ExceptionController.java
+++ app/src/main/java/com/msgsystems/jbugger/echipa2/backend/controllers/ExceptionController.java
@@ -1,5 +1,6 @@
 package com.msgsystems.jbugger.echipa2.backend.controllers;
 
+import com.msgsystems.jbugger.echipa2.backend.auth.DeactivatedUserException;
 import com.msgsystems.jbugger.echipa2.backend.auth.NotPermittedException;
 import com.msgsystems.jbugger.echipa2.backend.service.ServiceOperationException;
 import org.springframework.http.HttpStatus;
@@ -20,6 +21,12 @@ public class ExceptionController {
     protected ResponseEntity<Object> handleException(NotPermittedException ex) {
         System.out.println("NotPermittedEx");
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(value = { DeactivatedUserException.class })
+    protected ResponseEntity<Object> handleException(DeactivatedUserException ex) {
+        System.out.println("DeactivatedUserException");
+        return new ResponseEntity<>("User account is suspended.", HttpStatus.FORBIDDEN);
     }
 
 
